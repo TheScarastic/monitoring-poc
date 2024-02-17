0c12e21f4f45e996890a1b9979a0916a4550fa4a
TheScarastic <warabhishek@gmail.com>
2024-02-17 18:37:52 +0530

    Monitoring: Initial empty project

---
+++ app/src/main/java/me/abhishek/activitymonitoring/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.abhishek.activitymonitoring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
